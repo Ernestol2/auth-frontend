c4fd42793ef7a66a8503a3919be6120dfa593c96
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-04-03 08:35:15 -0400

    new redirect in register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import axios from "axios";
 
-
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const [success, setSuccess] = useState(false);
 
   const handleSubmit = (e) => {
     //prevent the form refreshing the page
@@ -23,10 +21,8 @@ export default function Register() {
     axios(config)
       .then((result) => {
         setMessage(result.data.message);
-        setSuccess(result.data.success);
-        if(success) { setTimeout(() => {
-          window.location.href = "/login";
-        }, 3000)}
+        (result.data.success) ? (setTimeout(() => {window.location.href = "/login"}, 3000))
+                              : (setTimeout(() => {window.location.href = "/register"}, 3000))
         
       })
       .catch((err) => {
@@ -34,51 +30,47 @@ export default function Register() {
       });
   };
 
-
-
   return (
-    <div className="register d-flex justify-content-center text-light"> 
-    <Col xs={8} sm={6} md={4} lg={3}>
-    <h2>Register</h2>
-    <Form onSubmit={(e) => handleSubmit(e)}>
-      {/* email */}
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter email"
-        />
-      </Form.Group>
+    <div className="register d-flex justify-content-center text-light">
+      <Col xs={8} sm={6} md={4} lg={3}>
+        <h2>Register</h2>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          {/* email */}
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+            />
+          </Form.Group>
 
-      {/* password */}
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-      </Form.Group>
+          {/* password */}
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </Form.Group>
 
-      {/* submit button */}
-      <Button
-        className="m-2"
-        variant="primary"
-        type="submit"
-        onClick={(e) => handleSubmit(e)}
-      >
-        Register
-      </Button>
-      { message ? (
-            <p>{message}</p>
-         ): <p></p> }
-    </Form>
-    </Col>
-  </div>
+          {/* submit button */}
+          <Button
+            className="m-2"
+            variant="primary"
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Register
+          </Button>
+          {message ? <p>{message}</p> : <p></p>}
+        </Form>
+      </Col>
+    </div>
   );
 }
